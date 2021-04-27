3ffe5835d9cddd64c2af2232f1bf32c10e1ab403
Jeff Cooper <jeff@southernworkstx.com>
2021-04-27 17:51:53 -0500

    add loadAudiofile to sampler

--- Sources/AudioKit/Nodes/Playback/Samplers/Sampler/Sampler.swift
+++ Sources/AudioKit/Nodes/Playback/Samplers/Sampler/Sampler.swift
@@ -327,6 +327,24 @@ public class Sampler: PolyphonicNode, AudioUnitContainer {
         self.loadSFZ(path: sfzPath, fileName: sfzFileName)
     }
 
+    // Public load function that makes sense on a sample player - simply takes an AVfile and loads that
+    public func loadAudioFile(file: AVAudioFile, rootNote: UInt8 = 48, noteFrequency: Float = 440,
+                              loKey: UInt8 = 0, hiKey: UInt8 = 127, loVelocity: UInt8 = 0, hiVelocity: UInt8 = 127,
+                              startPoint: Float = 0, endPoint: Float? = nil,
+                              loopEnabled: Bool = false, loopStartPoint: Float = 0, loopEndPoint: Float? = nil) {
+        let descriptor = SampleDescriptor(noteNumber: Int32(rootNote), noteFrequency: noteFrequency,
+                                          minimumNoteNumber: Int32(loKey), maximumNoteNumber: Int32(hiKey),
+                                          minimumVelocity: Int32(loVelocity), maximumVelocity: Int32(hiVelocity),
+                                          isLooping: loopEnabled,
+                                          loopStartPoint: loopStartPoint,
+                                          loopEndPoint: loopEndPoint ?? Float(file.length),
+                                          startPoint: startPoint,
+                                          endPoint: endPoint ?? Float(file.length))
+        unloadAllSamples()
+        loadAudioFile(from: descriptor, file: file)
+        buildKeyMap()
+    }
+
     internal func loadAudioFile(from sampleDescriptor: SampleDescriptor, file: AVAudioFile) {
         guard let floatChannelData = file.toFloatChannelData() else { return }
 
